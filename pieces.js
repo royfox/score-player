4f3d3941ee5f869c2cdc6d26c6d308f57fe29219
Roy Fox <roysfox@gmail.com>
2019-09-21 14:54:39 +0100

    MVP

---
+++ pieces.js
@@ -0,0 +1,57 @@
+var pieces = [
+    {
+        'pages': [884,  953,  1019, 1046, 1061, 1076, 1107, 1133, 1160, 1182,
+                  1204, 1226, 1248, 1277, 1304, 1314, 1341, 1367, 1405],
+        'pdfUrl' : 'IMSLP06801-Beethoven_-_Symphony_No.7_Mvt.II_(ed._Unger).pdf',
+        'videoId': '-4788Tmz9Zo',
+        'composer': 'Beethoven',
+        'title': 'Symphony No 7, 2nd Movement, Allegretto'
+    },
+    {
+        'pages': [32,  45,  60,  79,  92,  99,  105, 114, 120, 126,
+                  131, 139, 146, 159, 175, 189, 202, 214, 221, 228,
+                  234, 241, 249, 265, 291, 323, 333, 346, 356, 367,
+                  381, 394, 405, 415, 423, 430, 439, 452, 465, 480,
+                  488, 496, 500, 505, 515, 525, 531, 536, 541, 546,
+                  553, 559, 566, 572, 582, 588, 593, 598, 602],
+        'pdfUrl': 'IMSLP54076-PMLP08825-Dvorak_op.088_Sinfonie_Nr.8_1.Allegro_con_brio_fs_SNKLHU_3_8.pdf',
+        'videoId': 'QXAv-NGppFw',
+        'composer': 'Dvorak',
+        'title': 'Symphony No 8, 1st Movement, Allegro con brio'
+    },
+    {
+        'pages': [28,  33,  35,  52,  70,  84,  100, 140, 156, 170,
+                  181, 191, 205, 239, 258, 275, 296, 315, 333, 350,
+                  359, 368, 378, 385, 393, 403, 413, 423, 433, 442,
+                  455, 472, 489, 509, 526, 544, 561, 580, 597, 631,
+                  657, 668, 679, 690, 705, 720, 756
+                 ],
+        'pdfUrl': 'IMSLP13080-Mahler-Symphony_No.5_I.pdf',
+        'videoId': 'fEGNNuEM3Fc',
+        'composer': 'Mahler',
+        'title': 'Symphony No 5, 1st Movement, Trauermarsch'
+    }
+]
+pieces = pieces.map(piece => {
+    return {
+      ...piece,
+      name: piece.composer + " - " + piece.title,
+      slug: slugify(piece.composer + " " + piece.title)
+    };
+})
+
+// lifted from https://medium.com/@mhagemann/the-ultimate-way-to-slugify-a-url-string-in-javascript-b8e4a0d849e1
+function slugify(string) {
+  const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;'
+  const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnooooooooprrsssssttuuuuuuuuuwxyyzzz------'
+  const p = new RegExp(a.split('').join('|'), 'g')
+
+  return string.toString().toLowerCase()
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
+    .replace(/&/g, '-and-') // Replace & with 'and'
+    .replace(/[^\w\-]+/g, '') // Remove all non-word characters
+    .replace(/\-\-+/g, '-') // Replace multiple - with single -
+    .replace(/^-+/, '') // Trim - from start of text
+    .replace(/-+$/, '') // Trim - from end of text
+}
---
+++ scoreplayer.js
@@ -0,0 +1,218 @@
+pieces.sort((a, b) => a.name > b.name ? 1 : -1).forEach(function(piece) {
+    var select = document.getElementById("select-piece");
+    var option = document.createElement("option");
+    option.text = piece.name;
+    option.value = piece.slug;
+    select.appendChild(option);
+})
+
+
+
+function onYouTubeIframeAPIReady() {
+    YouTube.onIframeReady();
+}
+
+
+var YouTube = (function() {
+
+    var piece;
+    var player = null;
+
+    function getCurrentPageFromTime(currentTime) {
+        if(currentTime >= Math.max(...piece.pages)) {
+            return piece.pages.length;
+        }
+        for (i = 1; i < piece.pages.length; i++) {
+            if (currentTime < piece.pages[i]) {
+                return i;
+            }
+        }
+        return 1;
+    }
+
+    function setVideoTimeFromPage(page) {
+        if (page <= piece.pages.length) {
+            player.seekTo(piece.pages[page-1])
+        }
+    }
+
+    function resize() {
+        var spaceForVideo = window.innerWidth - document.getElementById("pdf-canvas").width
+        var width = Math.max(spaceForVideo - 50, 300)
+        var height = width / 1.64
+        player.setSize(width, height)
+    }
+
+    var resizeDebouncer;
+    window.addEventListener('resize', function(event){
+      clearTimeout(resizeDebouncer);
+      resizeDebouncer = setTimeout(function() {
+           PDF.resize()
+           resize()
+      }, 100);
+    });
+
+    setInterval(function() {
+        if(PDF.isReady() && document.getElementById("sync").checked) {
+            PDF.renderPage(getCurrentPageFromTime(player.getCurrentTime()))
+        }
+    }, 1000)
+
+    function init() {
+        var tag = document.createElement('script');
+
+        tag.src = "https://www.youtube.com/iframe_api";
+        var firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    }
+
+
+    return {
+        'onPlayerReady': function(event) {
+            event.target.playVideo();
+            player.seekTo(piece.pages[0])
+            PDF.init(piece['pdfUrl'])
+        },
+        'onIframeReady': function() {
+            player = new YT.Player('video', {
+                height: '390',
+                width: '640',
+                videoId: piece['videoId'],
+                events: {
+                    'onReady': YouTube.onPlayerReady
+                }
+            })
+        },
+        'setVideoTimeFromPage': function(page) {
+            if(document.getElementById("sync").checked) {
+                setVideoTimeFromPage(page);
+            }
+        },
+        'init': function(currentPiece) {
+            piece = currentPiece;
+            init();
+        },
+        'resize': function() {
+            resize();
+        }
+    }
+})();
+
+var PDF = (function() {
+
+    var pdfjsLib = window['pdfjs-dist/build/pdf'];
+    pdfjsLib.GlobalWorkerOptions.workerSrc = 'http://mozilla.github.io/pdf.js/build/pdf.worker.js';
+
+    var pdfDoc = null,
+        pageNum = 1,
+        pageRendering = false,
+        pageNumPending = null,
+        canvas = document.getElementById('pdf-canvas'),
+        ctx = canvas.getContext('2d');
+
+    function renderPage(num, forceRedraw=false) {
+
+        if(document.getElementById('page_num').textContent == num && !forceRedraw) {
+            return false;
+        }
+
+        pageRendering = true;
+        pdfDoc.getPage(num).then(function(page) {
+            var targetHeight = window.innerHeight;
+            var viewport = page.getViewport({ scale: 1, });
+            var scale = targetHeight / viewport.height;
+            var scaledViewport = page.getViewport({ scale: scale, });
+
+            canvas.width = scaledViewport.width;
+            canvas.height = scaledViewport.height;
+
+            var renderContext = {
+                canvasContext: ctx,
+                viewport: scaledViewport
+            };
+            var renderTask = page.render(renderContext);
+
+            renderTask.promise.then(function() {
+                pageRendering = false;
+                if (pageNumPending !== null) {
+                    renderPage(pageNumPending);
+                    pageNumPending = null;
+                }
+            });
+            YouTube.resize();
+        });
+        pageNum = num;
+        document.getElementById('page_num').textContent = num;
+
+    }
+
+
+    function queueRenderPage(num) {
+        if (pageRendering) {
+            pageNumPending = num;
+        } else {
+            renderPage(num);
+        }
+    }
+
+    function goToPage(page) {
+        if (page <= 1) {
+            return;
+        }
+         if (page > pdfDoc.numPages) {
+            return;
+        }
+        queueRenderPage(page);
+        YouTube.setVideoTimeFromPage(page)
+    }
+
+    function onPrevPage() {
+        goToPage(pageNum - 1);
+    }
+
+    function onNextPage() {
+        goToPage(pageNum + 1);
+    }
+
+    function enterPage() {
+        var targetNumber = parseInt(prompt("Go to page", pageNum), 10)
+        YouTube.setVideoTimeFromPage(targetNumber)
+    }
+
+    document.getElementById('prev').addEventListener('click', onPrevPage);
+    document.getElementById('next').addEventListener('click', onNextPage);
+    document.getElementById('page_num').addEventListener('click', enterPage);
+
+    return {
+        'init': function(url) {
+            pdfjsLib.getDocument(url).promise.then(function(pdfDoc_) {
+                pdfDoc = pdfDoc_;
+                document.getElementById('page_count').textContent = pdfDoc.numPages;
+                renderPage(pageNum);
+            });
+        },
+        'isReady': function() {
+            return pdfDoc != null;
+        },
+        'renderPage': function (page) {
+            queueRenderPage(page)
+        },
+        'resize': function () {
+            renderPage(pageNum, true)
+        }
+    }
+
+})();
+
+let urlPieces = window.location.search.substr(1).split("=");
+if(urlPieces.length == 2 && urlPieces[0] == "p") {
+    var piece = pieces.find(piece => piece['slug'] == urlPieces[1]);
+    window.document.title = piece['name']
+    document.getElementById("pdf-navigation").style.display = 'block';
+    document.getElementById("select-piece").value = piece.slug;
+    YouTube.init(piece)
+}
+
+
+
+
